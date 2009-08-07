79ec801fdbfc404126336ad3d55adb0cd4c68fb1
skitzycat@yahoo.com <skitzycat@yahoo.com>
2009-08-06 23:36:05 -0600

    added numpy conversion functions

--- beeutil.py
+++ beeutil.py
@@ -264,3 +264,70 @@ def norme(a,b):
 def print_debug(s):
 	if BEE_DEBUG:
 		print s
+
+# Functions to convert from numpy to qimage were taken from a post in the PyQt mailing list
+_bgra_rec = numpy.dtype({'b': (numpy.uint8, 0),
+	'g': (numpy.uint8, 1),
+	'r': (numpy.uint8, 2),
+	'a': (numpy.uint8, 3)})
+
+# convert image to numpy array
+def qimage2numpy(qimage):
+		if qimage.format() in (qtgui.QImage.Format_ARGB32_Premultiplied,
+													 qtgui.QImage.Format_ARGB32,
+													 qtgui.QImage.Format_RGB32):
+				dtype = _bgra_rec
+		elif qimage.format() == qtgui.QImage.Format_Indexed8:
+				dtype = numpy.uint8
+		else:
+				raise ValueError("qimage2numpy only supports 32bit and 8bit images")
+		# FIXME: raise error if alignment does not match
+		buf = qimage.bits().asstring(qimage.numBytes())
+		return numpy.frombuffer(buf, dtype).reshape(
+				(qimage.height(), qimage.width()))
+
+# convert from numpy array to qimage
+def numpy2qimage(array):
+	if numpy.ndim(array) == 2:
+		return gray2qimage(array)
+	elif numpy.ndim(array) == 3:
+		return rgb2qimage(array)
+	raise ValueError("can only convert 2D or 3D arrays")
+
+def gray2qimage(gray):
+	"""Convert the 2D numpy array `gray` into a 8-bit QImage with a gray
+	colormap.  The first dimension represents the vertical image axis."""
+	if len(gray.shape) != 2:
+		raise ValueError("gray2QImage can only convert 2D arrays")
+
+	gray = numpy.require(gray, numpy.uint8, 'C')
+
+	h, w = gray.shape
+
+	result = qtgui.QImage(gray.data, w, h, qtgui.QImage.Format_Indexed8)
+	result.ndarray = gray
+	for i in range(256):
+		result.setColor(i, qtgui.QColor(i, i, i).rgb())
+	return result
+
+def rgb2qimage(rgb):
+	"""Convert the 3D numpy array `rgb` into a 32-bit QImage.  `rgb` must
+	have three dimensions with the vertical, horizontal and RGB image axes."""
+	if len(rgb.shape) != 3:
+		raise ValueError("rgb2QImage can expects the first (or last) dimension to contain exactly three (R,G,B) channels")
+	if rgb.shape[2] != 3:
+		raise ValueError("rgb2QImage can only convert 3D arrays")
+
+	h, w, channels = rgb.shape
+
+	# Qt expects 32bit BGRA data for color images:
+	bgra = numpy.empty((h, w, 4), numpy.uint8, 'C')
+	bgra[...,0] = rgb[...,2]
+	bgra[...,1] = rgb[...,1]
+	bgra[...,2] = rgb[...,0]
+	bgra[...,3].fill(255)
+
+	result = qtgui.QImage(bgra.data, w, h, qtgui.QImage.Format_RGB32)
+	result.ndarray = bgra
+	return result
+
